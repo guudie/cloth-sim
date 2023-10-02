1a4e74807dd4f16a6c0bc428760837167452f703
guudie <gottychaos@gmail.com>
2023-10-02 20:24:44 +0700

    moved definitions to source files

--- mouse.h
+++ mouse.h
@@ -16,21 +16,21 @@ public:
     mouse(float mouseRadius = 20.0f) : pos({0, 0}), prevPos({0, 0}), radius(mouseRadius), leftButtonDown(false), rightButtonDown(false), sideButtonX2Down(false) { }
     ~mouse() = default;
 
-    const glm::vec2& getPos() const { return pos; }
-    const glm::vec2& getPrevPos() const { return prevPos; }
-    glm::vec2 getDiff() const { return pos - prevPos; }
+    const glm::vec2& getPos() const;
+    const glm::vec2& getPrevPos() const;
+    glm::vec2 getDiff() const;
     void updatePos(const glm::vec2& _pos);
     void updatePos(int x, int y);
 
-    float getMouseRadius() const { return radius; };
-    float getMouseRadius2() const { return radius * radius; }
+    float getMouseRadius() const;
+    float getMouseRadius2() const;
 
-    bool getLB() const { return leftButtonDown; }
-    void setLB(bool state) { leftButtonDown = state; }
+    bool getLB() const;
+    void setLB(bool state);
 
-    bool getRB() const { return rightButtonDown; }
-    void setRB(bool state) { rightButtonDown = state; }
+    bool getRB() const;
+    void setRB(bool state);
 
-    bool getSBX2() const { return sideButtonX2Down; }
-    void setSBX2(bool state) { sideButtonX2Down = state; }
+    bool getSBX2() const;
+    void setSBX2(bool state);
 };
